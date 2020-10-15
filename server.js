74961c3ceae57cd5299a433b4f4c5a1ee3abfab9
naphattharawat <naphattharawat@gmail.com>
2020-10-15 10:05:44 +0700

    name

--- server.js
+++ server.js
@@ -18,38 +18,48 @@ async function getConnection() {
     }
 }
 
-let dir = './backup';
-fse.ensureDirSync(dir)
-let dirBackup = path.join(dir, moment().format('YYYY_MM_DD'));
-fse.ensureDirSync(dirBackup)
-// fse.ensureDir(dir, err => {
-getConnection().then((result) => {
-    console.log('start backup');
-    for (const r of result) {
-        let fileName = '';
-        if (r.dbSaveName.length) {
-             fileName = r.dbSaveName + '.sql'
-        } else {
-            fileName = r.dbName + '.sql'
-        }
-        let filepath = path.join(dirBackup, fileName);
-        if (!shelljs.which('mysqldump')) {
-            shelljs.echo('Sorry, this script requires mysqldump');
-            shelljs.exit(1);
-        } else {
-            shelljs.echo(`Ok starting backup ${r.dbSaveName}`);
-            let cmd = `mysqldump --host=${r.dbHost} --port=3306 --add-drop-table ${r.dbName} --user=${r.dbUser} --password=${r.dbPassword} > ${filepath} `
-            shelljs.exec(cmd, async (code, stdout, stderr) => {
-                if (code !== 0) {
-                    shelljs.echo(`${dirBackup} - error ${r.dbSaveName}`);
-                } else {
-                    shelljs.echo(`${dirBackup}- Success ${r.dbSaveName}`);
-                }
-            });
-        }
-    }
-    console.log('Success backup');
+async function backup() {
+    try {
 
-});
+        let dir = './backup';
+        fse.ensureDirSync(dir)
+        let dirBackup = path.join(dir, moment().format('YYYY_MM_DD'));
+        fse.ensureDirSync(dirBackup)
+        // fse.ensureDir(dir, err => {
+        getConnection().then((result) => {
+            console.log('start backup');
+            for (const r of result) {
+                let fileName = '';
+                if (r.dbSaveName) {
+                    fileName = r.dbSaveName + '.sql'
+                } else {
+                    fileName = r.dbName + '.sql'
+                }
+                let filepath = path.join(dirBackup, fileName);
+                if (!shelljs.which('mysqldump')) {
+                    shelljs.echo('Sorry, this script requires mysqldump');
+                    shelljs.exit(1);
+                } else {
+                    shelljs.echo(`Ok starting backup ${fileName}`);
+                    let cmd = `mysqldump --host=${r.dbHost} --port=3306 --add-drop-table ${r.dbName} --user=${r.dbUser} --password=${r.dbPassword} > ${filepath} `
+                    shelljs.exec(cmd, async (code, stdout, stderr) => {
+                        if (code !== 0) {
+                            shelljs.echo(`${dirBackup} - error ${fileName}`);
+                        } else {
+                            shelljs.echo(`${dirBackup}- Success ${fileName}`);
+                        }
+                    });
+                }
+            }
+            console.log('Success backup');
+
+        });
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+backup();
 
 
